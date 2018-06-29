fb54baad173e669f4449ecd61ce301e87c3b395c
Luis Carrillo <luisr_carrillo@outlook.com>
2018-06-29 12:48:45 -0500

    Se elimino el target=_blank de la landing de conferencias

--- resources/views/landing/conferencia.blade.php
+++ resources/views/landing/conferencia.blade.php
@@ -218,7 +218,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="wow fadeInDown text-center" data-wow-delay="0.9s">
-                            <a href="#registrarme" target="_blank" >
+                            <a href="#registrarme">
                                 <button class="btn btn-default btn-lg">Reservar mi lugar</button>
                             </a>
                             <p><span>Cupo limitado</span></p>
