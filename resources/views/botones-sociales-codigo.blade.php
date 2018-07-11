86820368c9aac62188d31a2a1468e3c24d864f12
Luis Carrillo <luisr_carrillo@outlook.com>
2018-07-11 11:25:06 -0500

    Se creo un archivo con los codigos necesarios para cuando se ocupe agregar los botones sociales en los eventos en calendario.

---
+++ resources/views/botones-sociales-codigo.blade.php
@@ -0,0 +1,42 @@
+{{-- Código para botones de redes sociales en cursos/conferencias/presentaciones --}}
+
+<!-- Botones -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <!-- Twitter Tweet -->
+                    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false" >Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                      
+                    <!-- Facebook Like $ Share -->
+                    <div class="facebook" style="display: inline;  position: relative;  top: -3px;">
+	                    <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
+
+	                    <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Compartir</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!-- Script Facebook -->
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+<!-- Meta Datos -->
+
+	<meta property="og:url" content="https://www.inelco.mx/promociones" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Promociones del Mes de Inelco IT Solutions" />
+    <meta property="og:description" content="Aprovecha nuestras promociones del mes." />
+    <meta property="og:image" content="http://www.inelco.mx/images-new/inelco/promocion/social-media/inelco-promo-social-media.png" />
+    <meta property="og:image:secure_url" content="https://www.inelco.mx/images-new/inelco/promocion/social-media/inelco-promo-social-media.png" />
+    <link href="https://www.inelco.mx/images-new/inelco/promocion/social-media/inelco-promo-social-media.png" rel="image_src" />
